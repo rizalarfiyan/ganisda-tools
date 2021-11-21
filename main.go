9d0116c0289132c6cffb73a2304faaf858c7b8be
Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2021-11-21 10:51:59 +0700

    feat: add command pdf and mail

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		Commands: []*cli.Command{
+			{
+				Name:    "pdf",
+				Aliases: []string{"p"},
+				Usage:   "run command for make pdf files",
+				Action: func(c *cli.Context) error {
+					fmt.Println("run command for make pdf files")
+					return nil
+				},
+			},
+			{
+				Name:    "mail",
+				Aliases: []string{"m"},
+				Usage:   "run command for sender email",
+				Action: func(c *cli.Context) error {
+					var files string
+					if c.NArg() > 0 {
+						files = c.Args().Get(0)
+					}
+					if files == "" {
+						return errors.New("missing argument 1, file excel does'n exist")
+					}
+					fmt.Println("run command for sender email")
+					return nil
+				},
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
