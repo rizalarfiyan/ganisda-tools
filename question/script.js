57f344a2ee2465348bfd32dcbd31f5f77dd6a9ea
Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2021-12-11 17:32:34 +0700

    feat: add icons components

--- question/script.js
+++ question/script.js
@@ -1,3 +1,9 @@
+const icons = {
+  times: 'M13.41,12l4.3-4.29a1,1,0,1,0-1.42-1.42L12,10.59,7.71,6.29A1,1,0,0,0,6.29,7.71L10.59,12l-4.3,4.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L12,13.41l4.29,4.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42Z',
+  setting: 'M21.32,9.55l-1.89-.63.89-1.78A1,1,0,0,0,20.13,6L18,3.87a1,1,0,0,0-1.15-.19l-1.78.89-.63-1.89A1,1,0,0,0,13.5,2h-3a1,1,0,0,0-.95.68L8.92,4.57,7.14,3.68A1,1,0,0,0,6,3.87L3.87,6a1,1,0,0,0-.19,1.15l.89,1.78-1.89.63A1,1,0,0,0,2,10.5v3a1,1,0,0,0,.68.95l1.89.63-.89,1.78A1,1,0,0,0,3.87,18L6,20.13a1,1,0,0,0,1.15.19l1.78-.89.63,1.89a1,1,0,0,0,.95.68h3a1,1,0,0,0,.95-.68l.63-1.89,1.78.89A1,1,0,0,0,18,20.13L20.13,18a1,1,0,0,0,.19-1.15l-.89-1.78,1.89-.63A1,1,0,0,0,22,13.5v-3A1,1,0,0,0,21.32,9.55ZM20,12.78l-1.2.4A2,2,0,0,0,17.64,16l.57,1.14-1.1,1.1L16,17.64a2,2,0,0,0-2.79,1.16l-.4,1.2H11.22l-.4-1.2A2,2,0,0,0,8,17.64l-1.14.57-1.1-1.1L6.36,16A2,2,0,0,0,5.2,13.18L4,12.78V11.22l1.2-.4A2,2,0,0,0,6.36,8L5.79,6.89l1.1-1.1L8,6.36A2,2,0,0,0,10.82,5.2l.4-1.2h1.56l.4,1.2A2,2,0,0,0,16,6.36l1.14-.57,1.1,1.1L17.64,8a2,2,0,0,0,1.16,2.79l1.2.4ZM12,8a4,4,0,1,0,4,4A4,4,0,0,0,12,8Zm0,6a2,2,0,1,1,2-2A2,2,0,0,1,12,14Z',
+  history: 'M12,2A10,10,0,0,0,5.12,4.77V3a1,1,0,0,0-2,0V7.5a1,1,0,0,0,1,1H8.62a1,1,0,0,0,0-2H6.22A8,8,0,1,1,4,12a1,1,0,0,0-2,0A10,10,0,1,0,12,2Zm0,6a1,1,0,0,0-1,1v3a1,1,0,0,0,1,1h2a1,1,0,0,0,0-2H13V9A1,1,0,0,0,12,8Z'
+};
+
 function draggable(element) {
   let isMouseDown = false;
   let mouseX = 0;
@@ -33,6 +39,12 @@ class QuestionController {
   defaultOptions = {
     commentElement: "#stack",
     dragElement: "#drag",
+    iconLibrary: Object.create(null),
+    iconElement: "span[data-icon]",
+    iconAttrName: 'data-icon',
+    iconAttrSize: 'data-size',
+    defaultIcon: "times",
+    defaultIconSize: 24,
   };
 
   constructor(options) {
@@ -40,6 +52,7 @@ class QuestionController {
 
     this.el = document.querySelector(this.options.commentElement);
     this.dragElement = document.querySelector(this.options.dragElement);
+    this.iconsElement = document.querySelectorAll(this.options.iconElement);
 
     this.itemsId = [];
     this.items = [];
@@ -51,6 +64,22 @@ class QuestionController {
   _init() {
     this.showNoComment();
     draggable(this.dragElement);
+    this.createIcons(); 
+  }
+
+  createIcons() {
+    if (this.options.iconLibrary[this.options.defaultIcon] === undefined) {
+      console.warn('Icon library not loaded!')
+      return
+    }
+
+    this.iconsElement.forEach(elem => {
+      const getIconSize = elem.getAttribute(this.options.iconAttrSize) || this.options.defaultIconSize
+      const getIconName = elem.getAttribute(this.options.iconAttrName)
+      const iconName = this.options.iconLibrary[getIconName] || this.options.iconLibrary[this.options.defaultIcon]
+      const baseSvg = `<svg xmlns="http://www.w3.org/2000/svg" class="icon" width="${getIconSize}" height="${getIconSize}" viewBox="0 0 24 24" fill="currentColor"><path d="${iconName}"></path></svg>`
+      elem.insertAdjacentHTML('beforeend', baseSvg)
+    });
   }
 
   update(data) {
@@ -186,7 +215,9 @@ class QuestionController {
   }
 }
 
-const stack = new QuestionController();
+const stack = new QuestionController({
+  iconLibrary: icons
+});
 
 let data = [];
 const generateId = (long) => {
